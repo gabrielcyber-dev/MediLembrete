836f521996e7e04b5c697bef67bb54c4b64ca0ab
Gabriel Esperidião <gabrielsan.esperidiao@gmail.com>
2026-08-25 22:41:02 -0300

    Atualiza estrutura inicial do aplicativo

--- app/src/main/java/com/example/medilembrete/MainActivity.java
+++ app/src/main/java/com/example/medilembrete/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.medilembrete;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button; // necessário para criar botões
+import android.widget.Toast; // cria mensagem temporária
 
+// cria uma classe chamada "Main Activity" que herda Activity
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // carrega a parte visual
+        
+        Button btnAdicionar = findViewById(R.id.btnAdicionar); // chama o botão via id
+        btnAdicionar.setOnClickListener(v -> {
+            Toast.makeText(this, "Botão clicado!", Toast.LENGTH_SHORT).show();
+        });
     }
+
+    
 }
